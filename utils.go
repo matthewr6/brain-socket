a7d011be51afd08e5c5ed7a6bccc6451927d6885
Firedrake969 <firedrake969@users.noreply.github.com>
2017-01-16 14:11:40 -0700

    better way to get outputs

--- utils.go
+++ utils.go
@@ -7,27 +7,39 @@ import (
     "strings"
 )
 
-func SerializeOutputs(net *brain.Network) map[string][][]float64 {
+func SerializeOutputs(net *brain.Network) map[string]map[string][]float64 {
     // todo possibly refactor brain structure to allow for references from sensor to brain?
-    // todo - use only first part of name instead of -one and -two
     outputSubLevel := map[string][]float64{}
     outputsUsed := make(map[string]bool)
     for name, _ := range net.Outputs {
         baseName := name[0:len(name)-2]
         if _, exists := outputsUsed[baseName]; !exists {
-            // todo - gather all 0 through N
-            outputSubLevel[baseName] = []float64{net.Outputs[fmt.Sprintf("%v-0", baseName)].Value, net.Outputs[fmt.Sprintf("%v-1", baseName)].Value}
+            subLevel := []float64{}
+            outputExists := true
+            counter := 0
+            for outputExists {
+                output, existsScoped := net.Outputs[fmt.Sprintf("%v-%v", baseName, counter)]
+                outputExists = existsScoped
+                if outputExists {
+                    subLevel = append(subLevel, output.Value)
+                }
+                counter += 1
+            }
+            outputSubLevel[baseName] = subLevel
             outputsUsed[baseName] = true
         }
     }
 
     outputsUsed = make(map[string]bool)
     // todo - use left/right keys instead of array?
-    outputs := make(map[string][][]float64)
+    outputs := make(map[string]map[string][]float64)
     for name, _ := range outputSubLevel {
         baseName := strings.Split(name, "-")[0]
         if _, exists := outputsUsed[baseName]; !exists {
-            outputs[baseName] = [][]float64{outputSubLevel[fmt.Sprintf("%v-one", baseName)], outputSubLevel[fmt.Sprintf("%v-two", baseName)]}
+            outputs[baseName] = map[string][]float64{
+                "left": outputSubLevel[fmt.Sprintf("%v-one", baseName)],
+                "right": outputSubLevel[fmt.Sprintf("%v-two", baseName)],
+            }
             outputsUsed[baseName] = true
         }
     }
