548577ad955c677144f029b36ec52d15cfde2706
Firedrake969 <firedrake969@users.noreply.github.com>
2017-04-23 19:02:24 -0600

    listen for gopher brain as well

--- deployment/listener.js
+++ deployment/listener.js
@@ -6,6 +6,15 @@ var spawn = require('child_process').spawn;
 
 var baseDir = '/home/matthew/projects/brain-socket';
 
+function build() {
+    execSync('git pull origin master', {cwd: baseDir});
+    execSync('go get -u all');
+    execSync('go build', {cwd: baseDir});
+    execSync('npm i', {cwd: `${baseDir}/client`});
+    execSync('webpack', {cwd: `${baseDir}/client`});
+}
+
+build();
 var mainProcess = spawn('./brain-socket', {cwd: baseDir});
 mainProcess.stdout.on('data', function(data) {
     process.stdout.write(data.toString());
@@ -19,21 +28,13 @@ app.use(bodyParser.json());
 app.post('/', function (req, res) {
     var event = req.body;
     // make sure it's a push to master
-    if (event.repository.full_name.toLowerCase() === 'firedrake969/brain-socket' && event.ref === 'refs/heads/master') {
+    var repoName = event.repository.full_name.toLowerCase();
+    var repoChecks = ['firedrake969/gopher-brain', 'firedrake969/brain-socket']
+    if (repoChecks.indexOf(repoName) > -1 && event.ref === 'refs/heads/master') {
         console.log('Start deployment');
 
+        build();
 
-        // pull
-        execSync('git pull origin master', {cwd: baseDir});
-
-        // build
-        execSync('go get -u all');
-        execSync('go build', {cwd: baseDir});
-        execSync('npm i', {cwd: `${baseDir}/client`});
-        execSync('webpack', {cwd: `${baseDir}/client`});
-
-        // run, but first...
-        // kill server if already running
         if (mainProcess) {
             mainProcess.kill();
         }
